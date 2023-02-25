45fc0ea8afa24c6ffd9d64100a2a6d5702a7fcba
ljoonal <ljoonal@ljoonal.xyz>
2023-02-25 21:28:42 +0200

    v0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 
 #![cfg_attr(nightly, feature(doc_cfg))]
 #![deny(clippy::all)]
-#![deny(unsafe_code)]
+#![forbid(unsafe_code)]
 #![deny(clippy::cargo)]
 #![warn(missing_docs)]
 #![deny(rustdoc::invalid_html_tags)]
@@ -36,19 +36,30 @@ pub enum RequestMethod {
 /// Data for a HTTP request & response
 pub trait Queryable<RequiredApiState, ResponseType: DeserializeOwned> {
 	/// The URL of the request
-	fn url(&self) -> String;
+	fn url(&self, state: &RequiredApiState) -> String;
 
 	/// The method to use for the request
 	///
 	/// Defaults to `GET`.
-	fn method(&self) -> RequestMethod {
+	fn method(&self, _state: &RequiredApiState) -> RequestMethod {
 		RequestMethod::Get
 	}
 
 	/// Creates a JSON body for the request
 	///
 	/// Defaults to no body.
-	fn body(&self) -> Option<serde_json::Result<Vec<u8>>> {
+	fn body(&self, _state: &RequiredApiState) -> Option<serde_json::Result<Vec<u8>>> {
 		None
+	}
+
+	/// Deserializes the API response into the struct, by default using `serde_json`.
+	/// Required to allow deserializing empty tuples for example,
+	/// because [`serde_json` considers empty values to not be valid JSON](https://github.com/serde-rs/json/issues/903).
+	///
+	/// # Errors
+	/// 
+	/// If deserializing fails
+	fn deserialize(&self, data: &[u8]) -> serde_json::Result<ResponseType> {
+		serde_json::from_slice(data)
 	}
 }
